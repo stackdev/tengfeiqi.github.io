1772a7441e073ac2821d6fe257dd64aa337857e8
TengfeiQi <QiTengfeiLove@gmail.com>
2013-05-29 20:10:44 +0800

    add video

--- js/Tengfei.js
+++ js/Tengfei.js
@@ -207,4 +207,26 @@ function map(obj, iterator, context) {
   return results;
 }
 
+//动态加载js文件
+function loadScript(url,callback){
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    
+    if (script.readyState) {
+        script.onreadystatechange = function () {
+            if (script.readyState == "loaded" || script.readyState == "complete") {
+                script.onreadystatechange = null;
+                callback();
+            }
+        }
+    } else {
+               script.onload = function(){
+                   callback();
+               }
+    }
+    
+    script.src = url;
+    document.getElementsByTagName("head")[0].appendChild("script");
+}
+
 })(window, document);
